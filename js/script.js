9ca99824e48f01cffd32f72168c08e330dc89e58
PaulDenis <d.paul@outlook.it>
2021-05-27 14:46:33 +0200

    Prima parte completata

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@
 
 // Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+var app = new Vue ({
+    el: "#root",
+    data: {
+        mails: []
+    },
+    mounted() {
+        for (let i=0; i < 10; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then ((response) => {
+                    // console.log(response);
+                    this.mails.push(response.data.response);
+                    // console.log(this.mails);
+                })
+        }
+    }
+})
